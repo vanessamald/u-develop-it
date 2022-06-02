4edf9216e8f88c0ac51851958e494cb6571e78af
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-06-02 16:22:40 -0500

    adding the database calls

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // import express 
 const express = require('express');
 
+const mysql = require('mysql2');
+
 // port designation and app expression
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -8,6 +10,24 @@ const app = express();
 // express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // mysql username,
+        user : 'root',
+        // mysql password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// query the database to test the connection
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // default response for any other request (Not found)
 app.use((req, res) => {
