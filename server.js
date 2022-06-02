1ee8fd5c9aa61a4d9740bb99703ea40a27594346
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-06-02 18:40:51 -0500

    create query for deleteand create operation

--- server.js
+++ server.js
@@ -25,8 +25,36 @@ const db = mysql.createConnection(
 );
 
 // query the database to test the connection
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
+//db.query(`SELECT * FROM candidates`, (err, rows) => {
+    //console.log(rows);
+//});
+
+// GET a single candidate
+db.query('SELECT * FROM candidates WHERE id =1', (err, row) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(row);
+});
+
+// delete a candidate
+//db.query(`DELETE FROM candidates WHERE id = ?` , 1, (err, result) => {
+    //if (err) {
+       // console.log(err);
+    //}
+    //console.log(result);
+//});
+
+// create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+            VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
 });
 
 // default response for any other request (Not found)
